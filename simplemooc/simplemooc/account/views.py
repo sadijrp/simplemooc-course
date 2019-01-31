42f71d70c47e3050069f7288889a071c93874dbe
Sadi Antonio Peruzzo Júnior <sadijrp@gmail.com>
2019-01-31 16:16:58 +0000

    Aula 37 - Adicionar campo email ao form de usuários

--- simplemooc/simplemooc/account/views.py
+++ simplemooc/simplemooc/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 from django.conf import settings
 
@@ -8,12 +8,12 @@ def register(request):
     context = {}
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     context["form"] = form
 
