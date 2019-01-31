be82fb1228d9096bec9be0b0fa3ef3f3b6157995
Sadi Antonio Peruzzo Júnior <sadijrp@gmail.com>
2019-01-31 20:44:07 +0000

    Aula 47 - Adicionando Custom User

--- simplemooc/simplemooc/account/forms.py
+++ simplemooc/simplemooc/account/forms.py
@@ -1,33 +1,45 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
+User = get_user_model()
 
-    email = forms.EmailField(label="E-mail")
+class RegisterForm(forms.ModelForm):
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
+    password1 = forms.CharField(label="Senha", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmação de Senha", 
+                        widget=forms.PasswordInput)
 
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Já existe um usuário com este email")
-        
-        return email
+    def clean_password2(self):
+        password1 = self.cleaned_data["password1"]
+        password2 = self.cleaned_data["password2"]
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                "As senhas informadas não conferem",
+                code="password_mismatch"
+            )
+
+            return password2
     
     def save(self, commit=True):    
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
+        user.set_password(self.cleaned_data["password1"])
 
         if commit:
             user.save()
         
         return user
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 class EditAccountForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name"]
+        fields = ["username", "email", "name"]
 
     def clean_email(self):
         email = self.cleaned_data["email"]
