eb477598ae9ea0a901da3746720f39866091961a
sadijrp <sadijrp@gmail.com>
2019-02-01 13:15:50 -0200

    Alterando para Postgres para deploy no Heroku

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'simplemooc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simplemoocdb',
+        'USER': 'simplemoocuser',
+        'PASSWORD': 'simplemooc-passwd',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
