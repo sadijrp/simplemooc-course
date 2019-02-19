e07a4a391ddecb86aa455c13a88b27e2088f5aa1
Sadi Antonio Peruzzo Júnior <sadijrp@gmail.com>
2019-02-19 14:19:53 +0000

    Criado app forum e modelos para as threads e replies

---
+++ simplemooc/simplemooc/forum/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.conf import settings
+
+from taggit.managers import TaggableManager
+
+
+class Thread(model.Models):
+    title = models.CharField('Título', max_length=100)
+    body = models.TextField('Mensagem')
+    views = models.IntegerField('Visualizações', blank=True, default=0)
+    answers = models.IntegerField('Respostas', blank=True, default=0)
+    tags = TaggableManager()
+
+    created_at = models.DateTimeField(
+        verbose_name='Criado em',
+        auto_now_add=True)
+    updated_at = models.DateTimeField(
+        verbose_name='Atualizado em',
+        auto_now=True)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Autor',
+        on_delete=models.CASCADE,
+        related_name='threads')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Tópico'
+        verbose_name_plural = 'Tópicos'
+        ordering = ['-updated_at']
+
+
+class Reply(models.Model):
+    reply = models.TextField('Resposta')
+    correct = models.BooleanField('Correta?', blank=True, default=False)
+
+    created_at = models.DateTimeField(
+        verbose_name='Criado em',
+        auto_now_add=True)
+    updated_at = models.DateTimeField(
+        verbose_name='Atualizado em',
+        auto_now=True)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Autor',
+        on_delete=models.CASCADE,
+        related_name='replies')
+
+    def __str__(self):
+        return self.reply[:100]
+
+    class Meta:
+        verbose_name = 'Resposta'
+        verbose_name_plural = 'Respostas'
+        ordering = ['-correct', 'created_at']
--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'taggit',
+
     'simplemooc.core',
     'simplemooc.courses',
     'simplemooc.account',
