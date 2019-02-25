b25a575e2e0a05b23ace90a71b467f27553129d3
Sadi Antonio Peruzzo Júnior <sadijrp@gmail.com>
2019-02-25 20:02:03 +0000

    Aula 80 - Ordenando a lista de foruns

--- simplemooc/simplemooc/forum/views.py
+++ simplemooc/simplemooc/forum/views.py
@@ -5,9 +5,19 @@ from .models import Thread
 
 
 class ForumView(ListView):
-    model = Thread
-    paginate_by = 10
+    paginate_by = 2
     template_name = 'index.html'
+
+    def get_queryset(self):
+        queryset = Thread.objects.all()
+        order = self.request.GET.get('order', '')
+
+        if order == 'views':
+            queryset = queryset.order_by('-views')
+        elif order == 'answers':
+            queryset = queryset.order_by('-answers')
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
